bd9777ab6429fb2bcfb4abf60012d23ddaeaf731
Sofia Vyatkina <sonia.vyatkina2@yandex.ru>
2024-01-18 00:51:30 +0300

    add open-api controller generation

--- src/main/kotlin/com/cinephilia/payment/controllers/PaymentController.kt
+++ src/main/kotlin/com/cinephilia/payment/controllers/PaymentController.kt
@@ -2,23 +2,24 @@ package com.cinephilia.payment.controllers
 
 import com.cinephilia.payment.commands.cancelPaymentCommand
 import com.cinephilia.payment.commands.createPaymentCommand
-import com.cinephilia.payment.commands.proceedPaymentCommand
 import com.cinephilia.payment.commands.rejectPaymentCommand
-import com.cinephilia.payment.dtos.CreatePaymentResponseDto
-import org.springframework.web.bind.annotation.*
-import ru.quipy.core.EventSourcingService
 import com.cinephilia.payment.dtos.CreatePaymentRequestDto
-import com.cinephilia.payment.dtos.ProceedPaymentDto
+import com.cinephilia.payment.dtos.CreatePaymentResponseDto
 import com.cinephilia.payment.dtos.RejectPaymentDto
 import com.cinephilia.payment.enitites.PaymentAggregate
 import com.cinephilia.payment.enitites.PaymentAggregateState
+import com.stripe.exception.SignatureVerificationException
+import com.stripe.model.Event
+import com.stripe.net.Webhook
+import org.springframework.web.bind.annotation.*
+import ru.quipy.core.EventSourcingService
 import java.util.*
 
 @RestController
 @RequestMapping("/payments")
-class PaymentController(
+class xPaymentController(
         val PaymentEsService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
-) {
+): PaymentApi {
 
     @PutMapping
     fun createPayment(@RequestBody dto: CreatePaymentRequestDto): CreatePaymentResponseDto {
@@ -39,10 +40,28 @@ class PaymentController(
         }
     }
 
-    @PutMapping("/{id}/proceed")
-    fun proceedPayment(@PathVariable id: UUID, @RequestBody dto: ProceedPaymentDto) {
-        PaymentEsService.update(id) {
-            it.proceedPaymentCommand(id, dto.description)
+    var created : Boolean = false
+    @PostMapping("/proceed")
+    fun proceedPayment(@RequestBody payload: String, @RequestHeader("Stripe-Signature") sigHeader: String) {
+        val endpointSecret = "whsec_95e2a691360d96b09df1403804b329991c6dff44872dca42b15bf05175fe7129"
+
+        var event: Event? = null
+
+        // Verify webhook signature and extract the event.
+        // See https://stripe.com/docs/webhooks#verify-events for more information.
+
+        // Verify webhook signature and extract the event.
+        // See https://stripe.com/docs/webhooks#verify-events for more information.
+        event = Webhook.constructEvent(
+            payload, sigHeader, endpointSecret
+        )
+        println(event.type)
+        if (event.type.equals("payment_intent.created")){
+            created = true
+        } else {
+            if (!created) {
+                throw Exception()
+            }
         }
     }
 
