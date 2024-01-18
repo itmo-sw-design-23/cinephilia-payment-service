1dd2a6917f1df7d1195d901f2d18414af525bf8c
Mikhail Rukovishnikov <misha.rukovishnikow@yandex.ru>
2024-01-18 22:15:04 +0300

    Fix controller

--- src/main/kotlin/com/cinephilia/payment/controllers/PaymentController.kt
+++ src/main/kotlin/com/cinephilia/payment/controllers/PaymentController.kt
@@ -1,74 +1,80 @@
 package com.cinephilia.payment.controllers
 
-import com.cinephilia.payment.commands.cancelPaymentCommand
-import com.cinephilia.payment.commands.createPaymentCommand
-import com.cinephilia.payment.commands.rejectPaymentCommand
-import com.cinephilia.payment.dtos.CreatePaymentRequestDto
-import com.cinephilia.payment.dtos.CreatePaymentResponseDto
-import com.cinephilia.payment.dtos.RejectPaymentDto
 import com.cinephilia.payment.enitites.PaymentAggregate
 import com.cinephilia.payment.enitites.PaymentAggregateState
-import com.stripe.exception.SignatureVerificationException
-import com.stripe.model.Event
-import com.stripe.net.Webhook
-import org.springframework.web.bind.annotation.*
+import com.cinephilia.payment.model.CreatePaymentRequestDto
+import com.cinephilia.payment.model.CreatePaymentResponseDto
+import com.cinephilia.payment.model.RejectPaymentDto
+import org.springframework.http.ResponseEntity
 import ru.quipy.core.EventSourcingService
 import java.util.*
 
-@RestController
-@RequestMapping("/payments")
-class xPaymentController(
-        val PaymentEsService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
-): PaymentApi {
+class PaymentController(
+    val PaymentEsService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
+) : PaymentApi {
 
-    @PutMapping
-    fun createPayment(@RequestBody dto: CreatePaymentRequestDto): CreatePaymentResponseDto {
-        val payment = PaymentEsService.create {it.createPaymentCommand(user = dto.user, movie = dto.movie) }
 
-        return CreatePaymentResponseDto(payment.id);
+    //    @PutMapping
+//    fun createPayment(@RequestBody dto: CreatePaymentRequestDto): CreatePaymentResponseDto {
+//        val payment = PaymentEsService.create {it.createPaymentCommand(user = dto.user, movie = dto.movie) }
+//
+//        return CreatePaymentResponseDto(payment.id);
+//    }
+//
+//    @GetMapping("/{id}")
+//    fun getPayment(@PathVariable id: UUID): PaymentAggregateState? {
+//        return PaymentEsService.getState(id)
+//    }
+//
+//    @PutMapping("/{id}/cancel")
+//    fun cancelPayment(@PathVariable id: UUID) {
+//        PaymentEsService.update(id) {
+//            it.cancelPaymentCommand(id)
+//        }
+//    }
+//
+//    var created : Boolean = false
+//    @PostMapping("/proceed")
+//    fun proceedPayment(@RequestBody payload: String, @RequestHeader("Stripe-Signature") sigHeader: String) {
+//        val endpointSecret = "whsec_95e2a691360d96b09df1403804b329991c6dff44872dca42b15bf05175fe7129"
+//
+//        var event: Event? = null
+//
+//        // Verify webhook signature and extract the event.
+//        // See https://stripe.com/docs/webhooks#verify-events for more information.
+//
+//        // Verify webhook signature and extract the event.
+//        // See https://stripe.com/docs/webhooks#verify-events for more information.
+//        event = Webhook.constructEvent(
+//            payload, sigHeader, endpointSecret
+//        )
+//        println(event.type)
+//        if (event.type.equals("payment_intent.created")){
+//            created = true
+//        } else {
+//            if (!created) {
+//                throw Exception()
+//            }
+//        }
+//    }
+
+    override fun cancelPayment(id: UUID): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
     }
 
-    @GetMapping("/{id}")
-    fun getPayment(@PathVariable id: UUID): PaymentAggregateState? {
-        return PaymentEsService.getState(id)
+    override fun createPayment(createPaymentRequestDto: CreatePaymentRequestDto): ResponseEntity<CreatePaymentResponseDto> {
+        TODO("Not yet implemented")
     }
 
-    @PutMapping("/{id}/cancel")
-    fun cancelPayment(@PathVariable id: UUID) {
-        PaymentEsService.update(id) {
-            it.cancelPaymentCommand(id)
-        }
+    override fun getPaymentById(id: UUID): ResponseEntity<com.cinephilia.payment.model.PaymentAggregateState> {
+        TODO("Not yet implemented")
     }
 
-    var created : Boolean = false
-    @PostMapping("/proceed")
-    fun proceedPayment(@RequestBody payload: String, @RequestHeader("Stripe-Signature") sigHeader: String) {
-        val endpointSecret = "whsec_95e2a691360d96b09df1403804b329991c6dff44872dca42b15bf05175fe7129"
-
-        var event: Event? = null
-
-        // Verify webhook signature and extract the event.
-        // See https://stripe.com/docs/webhooks#verify-events for more information.
-
-        // Verify webhook signature and extract the event.
-        // See https://stripe.com/docs/webhooks#verify-events for more information.
-        event = Webhook.constructEvent(
-            payload, sigHeader, endpointSecret
-        )
-        println(event.type)
-        if (event.type.equals("payment_intent.created")){
-            created = true
-        } else {
-            if (!created) {
-                throw Exception()
-            }
-        }
+    override fun proceedPayment(body: String) {
+        TODO("Not yet implemented")
     }
 
-    @PutMapping("/{id}/reject")
-    fun rejectPayment(@PathVariable id: UUID, @RequestBody dto: RejectPaymentDto) {
-        PaymentEsService.update(id) {
-            it.rejectPaymentCommand(id, dto.description)
-        }
+    override fun rejectPayment(id: UUID, rejectPaymentDto: RejectPaymentDto): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
     }
 }
