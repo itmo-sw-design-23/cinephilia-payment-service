30d4ed980423f85042cceb6d9f6a738dcec41868
Mikhail Rukovishnikov <misha.rukovishnikow@yandex.ru>
2024-03-19 23:09:19 +0300

    Add swagge

--- src/main/kotlin/com/cinephilia/payment/CinephiliaPaymentServiceApplication.kt
+++ src/main/kotlin/com/cinephilia/payment/CinephiliaPaymentServiceApplication.kt
@@ -1,9 +1,18 @@
 package com.cinephilia.payment
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication()
+@OpenAPIDefinition(
+	info = Info(
+		title = "Cinephilia's Payments API",
+		version = "1.0.0",
+		description = "Payments API for Cinephilia's streaming service"
+	)
+)
 class CinephiliaPaymentServiceApplication
 
 fun main(args: Array<String>) {
